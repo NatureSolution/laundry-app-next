0b05027cebe55404ae10ce70dca7f96259ac4e26
syadur-killa <syadur.killa@gmail.com>
2023-12-31 22:26:04 +0600

    commit from home pc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+// import NavbarPage from '@/components/navbar/Navbar'
+import Footer from '@/components/footer/footer'
+import NavbarPage from '@/components/navbar/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavbarPage/>
+        {children}
+        <Footer/>
+        </body>
     </html>
   )
 }
