f1a4f7632c4426369f129753995994a5c3827eda
syadur-killa <syadur.killa@gmail.com>
2024-01-03 22:56:05 +0600

    'test'

---
+++ src/app/home/feature/featurepage.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import icon1 from "../../../../public/icon/accelerate-svgrepo-com.svg";
+import icon2 from "../../../../public/icon/trend-analysis-svgrepo-com.svg";
+import icon3 from "../../../../public/icon/format-painter-svgrepo-com.svg";
+import Image from "next/image";
+
+const FeaturePage = () => {
+  return (
+    <div className="bg-black text-white">
+      <div className="m-3 py-10">
+        <h1 className="text-xl bold">
+          Every feature you need to win. Try it for yourself.
+        </h1>
+        <p className="text-sm">
+          Pocket was built for investors like you who play by their own rules
+          and aren’t going to let SEC regulations get in the way of their
+          dreams. If other investing tools are afraid to build it, Pocket has
+          it.
+        </p>
+      </div>
+      <div className="flex p-4">
+        <div className="mx-20">
+          <div className="mockup-phone">
+            <div className="camera"></div>
+            <div className="display">
+              <div className="artboard artboard-demo phone-1">Hi.</div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="my-7 p-3 hover:bg-gray-900 border rounded-lg">
+            <Image src={icon1} width="40" height="40" placeholder="empty" />
+            <h1>Lorem ipsum dolor sit .</h1>
+            <p className="text-xs">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
+              saepe placeat doloremque tempora atque, nam natus quam sequi
+            </p>
+          </div>
+          <div className="my-7 p-3 hover:bg-gray-900 border rounded-lg">
+            <Image src={icon3} width="40" height="40" placeholder="empty" />
+            <h1>Lorem ipsum dolor sit .</h1>
+            <p className="text-xs">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
+              saepe placeat doloremque tempora atque, nam natus quam sequi
+            </p>
+          </div>
+          <div className="my-7 p-3 hover:bg-gray-900 border rounded-lg">
+            <Image src={icon2} width="40" height="40" placeholder="empty" />
+            <h1>Lorem ipsum dolor sit .</h1>
+            <p className="text-xs">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
+              saepe placeat doloremque tempora atque, nam natus quam sequi
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturePage;
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,14 @@
 "use client"
 import React from "react";
 import ServiceStatus from "../masculinious/servicestatus/pags";
+import Link from "next/link";
+import cover  from "../../../public/images/cover0.png";
+import i1 from '../../../public/images/bbc.9cfc75a9.svg'
+import i2 from '../../../public/images/cbs.aa596395.svg'
+import i3 from '../../../public/images/cnn.eaae184a.svg'
+import i4 from '../../../public/images/bbc.9cfc75a9.svg'
+import Image  from "next/image";
+import FeaturePage from "./feature/featurepage";
 
 export default function HomePage() {
     const handleClick = () => {
@@ -9,19 +17,38 @@ export default function HomePage() {
 
     return (
       <div>
-        <div className="flex justify-center py-5">
-          <div className="flex">
-            <div className="text-6xl bold">Let get Out</div>
+        <div className="flex pt-14 m-7">
+          <div>
             <div>
-            <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-
+              <h1 className="text-3xl font-extrabold">Cutting Edge <br /> <span className="text-violet-700">Technology</span>  <br />Services</h1>
+              <p>The cutting edge solution for the technology advancement for the leading company in the country.</p>
+            </div>
+            <div className="flex py-7 gap-4">
+              <div>
+                <Link href={'/services'}><button className="btn btn-outline">
+                Services
+                </button></Link></div>
+              <div> <Link href={'/about'}><button className="btn btn-outline btn-accent">About</button></Link> </div>
             </div>
           </div>
-          <div>Test</div>
+          <div>
+            <Image src={cover} width='700px' height='700px'  placeholder="blur"/>
+          </div>
         </div>
-        <div className="flex justify-center py-10">
-        <ServiceStatus/>
+        <div className=" flex flex-wrap mx-7 gap-2">
+        <Image src={i1} width='8px' height='4px'  placeholder="empty"/>
+        <Image src={i2} width='8px' height='4px'  placeholder="empty"/>
+        <Image src={i3} width='8px' height='4px'  placeholder="empty"/>
+        <Image src={i1} width='8px' height='4px'  placeholder="empty"/>
+        <Image src={i1} width='8px' height='4px'  placeholder="empty"/>
+        <Image src={i1} width='8px' height='4px'  placeholder="empty"/>
+        
         </div>
+        <div><FeaturePage/></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        
       </div>
       
     )
